20cde2fdf624940ff9be878dd14005385f3b4b73
AndreMiller97 <andre.miller97@hotmail.com>
2023-01-13 21:08:39 -0300

    feat: implement header at home page

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 // fazendo requisição na API para validar se o email existe ou não
 const validateUser = async (email) =>{
     try {
+        //função fetch do tipo get para fazer a chamada da api 
         const result = await fetch(
+            // utilizando a url padrao da api para consulta passando a rota e utilizando query também passando o parametro (email)
             `https://mp-wallet-app-api.herokuapp.com/users?email=${email}`
             )
+            // variavel de usuario onde recebe o resultado .json  que vem da do fetch (url do back-and)
             const user = await result.json()
+            // retorna o usuario se deu tudo certo
             return user
     } catch (error) {
         return {error}
     }
 }
 
-// validação de e-mail valido ou invalido
+// validação de e-mail valido ou invalido utilizando async await
 const onClickLogin = async () =>{
     const email = document.getElementById("input-email").value;
     if (email.length <5 || !email.includes('@')){
         alert("E-mail invalido!")
         return;
     }
+    //fazendo a promisse do validateUser passando o email para fazer a comunicação
     const result = await validateUser(email)
     if(result.error){
         alert("Falha ao validar E-mail!")
         return
     }
-    // setando o e-mail para fazer o login
+    // setando o e-mail, name e id para fazer o login
     localStorage.setItem("@WalletApp:userEmail", result.email)
     localStorage.setItem("@WalletApp:userName", result.name)
     localStorage.setItem("@WalletApp:userId", result.id)
--- src/pages/register/cadastrar.js
+++ src/pages/register/cadastrar.js
@@ -1,12 +1,15 @@
+
 // chamando API para cadastrar com metodo post
 const onCallRegister = async (email, name) =>{
     try {
+        // passando o body via parametro 
         const data = {
             email,
             name,
         }
-        
+        // função fetch do tipo post
         const response = await fetch (
+            // url padrao passando a rota de usurarios (users)
             "https://mp-wallet-app-api.herokuapp.com/users",
             {
                 // cabeçalho para o metodo post
@@ -15,12 +18,16 @@ const onCallRegister = async (email, name) =>{
                 cache: "no-cache",
                 credentials:"same-origin",
                 headers:{
+                    // headers serve para mandar um objeto java scrit para o body
                     "Content-Type": "application/json",
                 },
+                // passando o json(objeto) em formato de texto para poder conseguir trafegar na web
                 body: JSON.stringify(data),
             }
         );
+        // variavel de usuario onde recebe o resultado .json  que vem da do fetch (url do back-and)
         const user = await response.json();
+        // retorna o usuario se deu tudo certo
         return user;
 
     } catch (error) {
@@ -28,11 +35,13 @@ const onCallRegister = async (email, name) =>{
     }
 }
 
-//validando nome e email para poder cadastrar
+// função para cadastrar async await
 const onResgister = async () => {
+    // pegando nome e email via id
     const name = document.getElementById("input-name").value
     const email = document.getElementById("input-email").value;
 
+    // validando nome e email para poder cadastrar
     if (name.length < 3 ){
         alert("O nome deve conter mais do que 3 caracteres.")
         return;
@@ -42,13 +51,15 @@ const onResgister = async () => {
         alert("O E-mail invalido")
         return;
     }
-
+    //fazendo a promisse da resposta da API passando o email e nome para fazer a comunicação
     const result = await onCallRegister(email,name);
 
     if (result.error) {
         alert("Falha ao cadastrar")
         return;
     }
+
+    // setando o e-mail, name e id para fazer o login
     localStorage.setItem("@WalletApp:userEmail", result.email)
     localStorage.setItem("@WalletApp:userName", result.name)
     localStorage.setItem("@WalletApp:userId", result.id)
@@ -58,9 +69,13 @@ const onResgister = async () => {
 
 //função de submit para cadastrar
 window.onload = ()=>{
+    // pegando o elemendo via id
     const form = document.getElementById("form-cadastrar")
+
     form.onsubmit = (event) => {
         event.preventDefault();
+
+        //chamando a função para poder fazer o submit de cadastro
         onResgister()
     }
 }
